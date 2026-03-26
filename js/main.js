8d14ae869b085328c504cf43eaff61d3b0d09b74
brymmobaggins <ibro2tablet21@yahoo.com>
2026-03-26 16:22:52 +0100

    feat: add logic for invoic info

--- js/main.js
+++ js/main.js
@@ -2,12 +2,40 @@
 
 function init() {
   syncInputToPreview("business-name", "preview-business-name", "Business Name");
-  syncInputToPreview("business-address", "preview-business-address", "Business Address");
-  syncInputToPreview("business-email", "preview-business-email", "Business Email Address");
-  syncInputToPreview("business-phone", "preview-phone-number", "Business Phone Number");
+  syncInputToPreview(
+    "business-address",
+    "preview-business-address",
+    "Business Address",
+  );
+  syncInputToPreview(
+    "business-email",
+    "preview-business-email",
+    "Business Email Address",
+  );
+  syncInputToPreview(
+    "business-phone",
+    "preview-phone-number",
+    "Business Phone Number",
+  );
   syncInputToPreview("customer-name", "preview-customer-name", "Customer Name");
-  syncInputToPreview("customer-address", "preview-customer-address", "Customer Address");
+  syncInputToPreview(
+    "customer-address",
+    "preview-customer-address",
+    "Customer Address",
+  );
+
+  syncInputToPreview(
+    "invoice-number",
+    "preview-invoice-number",
+    "Invoice number",
+  );
+  syncInputToPreview(
+    "invoice-date",
+    "preview-invoice-date",
+    "Invoice Date",
+  );
 }
+init();
 
 function syncInputToPreview(inputId, previewId, fallback) {
   const input = document.getElementById(inputId);
@@ -18,4 +46,3 @@ function syncInputToPreview(inputId, previewId, fallback) {
   });
 }
 
-init();
